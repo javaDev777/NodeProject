7d3b37511be2736f371bed95a5f1c81036193e83
javaDev777 <Lolita.young777@gmail.com>
2022-06-29 18:33:34 -0400

    updates to complete the project

--- restApi/app.js
+++ restApi/app.js
@@ -4,10 +4,15 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+app.use(bodyParser.json());
 
 
 //Import Routes
 const postsRoute = require('./routes/posts');
+const router = require('./routes/posts');
 app.use('/posts',postsRoute);
 
 //Routes
@@ -15,11 +20,10 @@ app.get('/', (req, res) =>{
     res.send('We are on home');
 });
 
-//Connect to DB
-/* mongoose.connect(process.env.DB_CONNECTION, {userNewUrlParser: true}, () =>
+ //Connect to DB
+ mongoose.connect("mongodb+srv://lyoung777:Test1234!@cluster0.hxtgm.mongodb.net/Perficient?retryWrites=true&w=majority", {userNewUrlParser: true}, () =>
     console.log('connected to DB!')
-); */
-
+); 
 //How to we start listening to to the server
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`server is listening on: ${PORT} `)); 
